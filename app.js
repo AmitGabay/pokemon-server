1cca61a82f5e6e579d10e8eee81ac5515e685acb
Amit <amit.b.gabay@gmail.com>
2022-03-06 17:54:38 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import fs from "fs";
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app
+  .post("/pokemons", (req, res) => {
+    const pokemon = req.body;
+    const data = JSON.stringify(pokemon);
+
+    fs.writeFile("pokemon.json", data, (err) => {
+      if (err) throw err;
+      console.log("Data written to file");
+    });
+    res.sendStatus(200);
+  })
+  .get("/pokemons", (req, res) => {
+    fs.readFile("pokemon.json", (err, data) => {
+      if (err) throw err;
+      const pokemon = JSON.parse(data);
+      res.json(pokemon);
+    });
+  });
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
